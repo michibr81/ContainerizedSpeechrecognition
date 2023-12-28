687c8ef0e48a0ffb6f241cf4bc006f4149544478
michibr81 <michibr+git@posteo.de>
2023-12-28 09:19:11 +0100

    settings per json file & move unneccessary to archive folder

--- scripts/speech2mqtt.py
+++ scripts/speech2mqtt.py
@@ -1,15 +1,33 @@
 #!/usr/bin/env python3
 
-import speechEngine as se# NOTE: requires PyAudio because it uses the Microphone class
-import time
+#import speechEngine as se# NOTE: requires PyAudio because it uses the Microphone class
+#import time
 from pocketsphinx import LiveSpeech
-from pocketsphinx import get_model_path #, get_data_path
-import inspect
+from pocketsphinx import get_model_path
+#import inspect
 import paho.mqtt.client as mqtt
 import json
-import speechEngine as se
+#import speechEngine as se
 from enum import Enum
 import commandComposition as comp
+from pathlib import Path
+import os
+
+def make_relative_path_to_this(path):
+    if(os.path.isabs(path)):
+        return path
+    else:
+        base_path = Path(__file__).parent
+        return str((base_path / path).resolve())
+settings = None
+#base_path = Path(__file__).parent
+with open(make_relative_path_to_this("speech2mqtt.settings.json")) as f:
+    settings = json.loads(f.read())
+
+#Pocketsphinx
+hmmpath = make_relative_path_to_this(settings["PocketSphinx"]["hmm"])
+lmpath = make_relative_path_to_this(settings["PocketSphinx"]["lm"])
+dic = make_relative_path_to_this(settings["PocketSphinx"]["dic"])
 
 print("Livespeech before")
 speech = LiveSpeech(
@@ -20,65 +38,30 @@ speech = LiveSpeech(
     no_search=False,
     full_utt=False,
     verbose=False,
-    # hmm='/home/sphinx_models/cmusphinx-cont-voxforge-de/model_parameters/voxforge.cd_cont_6000',
-    # lm='/home/sphinx_models/cmusphinx-voxforge-de.lm.bin',
-    # dic='/home/sphinx_models/tinyde.dict'
-    hmm='/media/DATENLT/Programmierung/_GIT_REPOS/ContainerizedSpeechrecog/sphinx_models/cmusphinx-cont-voxforge-de/model_parameters/voxforge.cd_cont_6000',
-    lm='/media/DATENLT/Programmierung/_GIT_REPOS/ContainerizedSpeechrecog/sphinx_models/cmusphinx-voxforge-de.lm.bin',
-    dic='/media/DATENLT/Programmierung/_GIT_REPOS/ContainerizedSpeechrecog/sphinx_models/tinyde.dict'   
+    hmm=hmmpath,
+    lm=lmpath,
+    dic=dic
     )
 
-print("Livespeech started")
-print(type(speech.ad))
+#MQTT
+MQTT_SERVER = settings["Mqtt"]["Server"]
+MQTT_PATH = settings["Mqtt"]["Path"]
+MQTT_PORT = settings["Mqtt"]["Port"]
 
-MQTT_SERVER = 'MBraspi4'
-MQTT_PATH = "MBR/automation/shutters"
 
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+    print("Connected to mqtt with result code "+str(rc))
 client = mqtt.Client()
-client.on_connect = on_connect
- 
-client.connect(MQTT_SERVER, 1883, 60)
+client.on_connect = on_connect 
+client.connect(MQTT_SERVER, MQTT_PORT , 60)
 
-commandMapStr = """
-{
-    "Location":[
-        {"Bath":["bad","badezimmer"]},
-        {"Office":["büro"]},
-        {"All":["alle"]},
-        {"South":["süd","südseite","sonnenseite"]},
-        {"Living":["essstube","esstisch","esszimmer"]}
-    ],
-    "Direction":[        
-        {"Down":["runter","herrunter","zu","herab"]},
-        {"Up":["hoch","rauf","herauf"]}
-    ],
-    "Quantity":[
-        {"Half":["halb","teilweise"]},
-        {"Full":["komplett","ganz"]}
-    ]
-}
-"""
-
+#speech2mqtt
 class SPEECH_DETECTION_STATE(Enum):
     WAITING_FOR_KEYPHRASE = 1, #initial state
     WAITING_FOR_COMMAND = 2,
     PEFORM_COMMAND_ACTION = 3
 
-
-
-commandMap = json.loads(commandMapStr)
-keyphrase = 'computer'
-
-from pathlib import Path
-
-jsn = None
-base_path = Path(__file__).parent
-with open((base_path / "cmds.json").resolve()) as f:
-    jsn = json.loads(f.read())
-#     IterateCategories(jsn,["büro", "fenster","teilweise","runter"] )
-
+keyphrase = settings["Keyphrase"]
 
 state = SPEECH_DETECTION_STATE.WAITING_FOR_KEYPHRASE
 print(f"waiting for keyphrase '{keyphrase}'...")
@@ -92,32 +75,8 @@ for phrase in speech:
         print('keyphrase detected, waiting for command....')
         state = SPEECH_DETECTION_STATE.WAITING_FOR_COMMAND
     elif(state == SPEECH_DETECTION_STATE.WAITING_FOR_COMMAND):
-        #command = se.FindShutterCommand(recognizedwords, commandMap)
-        command = comp.IterateCategories(jsn,recognizedwords)
-        print(f"Detected command: {command}")
-        ret = client.publish(MQTT_PATH,command)
+        command = comp.IterateCategories(settings,recognizedwords)
+        if(command is not None):
+            print(f"Detected command: {command}")
+            ret = client.publish(MQTT_PATH,command)
         state = SPEECH_DETECTION_STATE.WAITING_FOR_KEYPHRASE
-
-
-#print(commandMap)
-
-# an for in loop to iterate in speech
-# for phrase in speech:
-#     print("waiting for keyphrase")
-#     recognizedSpeech = phrase.segments(detailed=True)
-#     print(f"TYPE ---------: {type(recognizedSpeech)}")
-#     print(recognizedSpeech)
-#     recognizedwords = [speechPart[0] for speechPart in recognizedSpeech]
-#     print("recognizedwords: " + ",".join(recognizedwords))
-#     if 'computer' in recognizedwords:
-#         print('keyphrase detected')
-#         print("waiting for command")
-#         for phrase in speech:
-#             recognizedSpeech = phrase.segments(detailed=True)
-#             print(f"recognizedWors TYPE: {type(recognizedwords)}")
-#             print(recognizedSpeech)
-#             recognizedwords = [speechPart for speechPart in recognizedSpeech]
-
-#             command = se.FindShutterCommand(recognizedwords, commandMap)
-#             print(command)
-#             ret= client.publish(MQTT_PATH,command)   
